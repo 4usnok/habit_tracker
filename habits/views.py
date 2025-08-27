9e3f4a91511f6ca1e615cfc0ab34aa67b8ebd510
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-08-27 03:48:26 +0300

    project_5: ex-3 -> pagination

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+from habits.models import Habit
+from habits.pagination import HabitPagination
+from habits.serializers import HabitSerializer
+
+
+class MyView(APIView):
+    pagination_class = HabitPagination
+
+    def get(self, request):
+        queryset = Habit.objects.all()
+        paginator = self.pagination_class()  # Создаем экземпляр пагинатора
+        paginated_queryset = paginator.paginate_queryset(
+            queryset, request
+        )  # Пагинируем queryset
+        serializer = HabitSerializer(paginated_queryset, many=True)  # Сериализируем
+        return paginator.get_paginated_response(
+            serializer.data
+        )  # Возвращаем пагинированный ответ
